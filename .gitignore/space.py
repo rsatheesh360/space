0b7b94cf211bc0f7de7a688cb8252588f6e63e0a
R.Satheesh <36471895+rsatheesh360@users.noreply.github.com>
2018-03-03 15:15:09 +0530

    count the space

---
+++ .gitignore/space.py
@@ -0,0 +1,12 @@
+string=input("Enter string:")
+char=0
+word=1
+for i in string:
+      char=char+1
+      if(i==' '):
+            word=word+1
+print("Number of words in the string:")
+print(word)
+print("Number of characters in the string:")
+print(char)
+pint(char)
